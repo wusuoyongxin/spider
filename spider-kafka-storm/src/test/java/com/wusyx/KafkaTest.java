17e850d087709c1649f8616dab5c7cb38eb8cbd4
wujianbo <wujianbo@gmsdtech.com>
2015-12-11 17:54:26 +0800

    kafka

---
+++ spider-kafka-storm/src/test/java/com/wusyx/KafkaTest.java
@@ -0,0 +1,15 @@
+package com.wusyx;
+
+import org.junit.Test;
+
+public class KafkaTest {
+
+    @Test
+    public void kafka() {
+        <dependency>
+        <groupId>org.apache.kafka</groupId>
+        <artifactId>kafka_2.10</artifactId>
+        <version>0.8.2.0</version>
+    </dependency>
+    }
+}
