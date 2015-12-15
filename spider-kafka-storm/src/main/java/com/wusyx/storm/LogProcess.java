58c8d394bdb6d560df3a5ce6282d2a3fa107d9c1
WuJianbo <9994474@qq.com>
2015-12-15 12:14:02 +0800

    kafka-storm

---
+++ spider-kafka-storm/src/main/java/com/wusyx/storm/LogProcess.java
@@ -0,0 +1,36 @@
+package com.wusyx.storm;
+
+import storm.kafka.BrokerHosts;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.ZkHosts;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+
+public class LogProcess {
+	
+	public static void main(String[] args) {
+		
+		TopologyBuilder topologyBuilder = new TopologyBuilder();
+		BrokerHosts hosts = new ZkHosts("192.168.199.110:2181");//表示kafka的地址，可以指定zookeeper地址也可以
+		String topic = "spider";//kafak中的主题
+		String zkRoot = "/spider";//zk的一个根目录
+		String id = "123";//是groupid
+		SpoutConfig spoutConf = new SpoutConfig(hosts, topic, zkRoot, id);
+		
+		String spout_id = KafkaSpout.class.getSimpleName();
+		String bolt_id = LogFilterBolt.class.getSimpleName();
+		
+		
+		topologyBuilder.setSpout(spout_id, new KafkaSpout(spoutConf ));
+		topologyBuilder.setBolt(bolt_id, new LogFilterBolt()).shuffleGrouping(spout_id);
+		
+		LocalCluster localCluster = new LocalCluster();
+		localCluster.submitTopology("topology-name", new Config(), topologyBuilder.createTopology());
+		
+		
+		
+	}
+
+}
--- spider-kafka-storm/src/test/java/com/wusyx/KafkaTest.java
+++ spider-kafka-storm/src/test/java/com/wusyx/KafkaTest.java
@@ -1,15 +1,29 @@
 package com.wusyx;
 
+import java.util.Properties;
+
 import org.junit.Test;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+import kafka.serializer.StringEncoder;
 
 public class KafkaTest {
 
-    @Test
-    public void kafka() {
-        <dependency>
-        <groupId>org.apache.kafka</groupId>
-        <artifactId>kafka_2.10</artifactId>
-        <version>0.8.2.0</version>
-    </dependency>
+    @SuppressWarnings("static-access")
+	@Test
+    public void kafka() throws InterruptedException {
+    	Properties originalProps = new Properties();
+		originalProps.put("metadata.broker.list", "192.168.199.110:9092");
+		originalProps.put("serializer.class", StringEncoder.class.getName());
+		ProducerConfig producerConfig = new ProducerConfig(originalProps );
+		Producer<String, String> producer = new Producer<String, String>(producerConfig);
+		while(true){
+			KeyedMessage<String, String> message = new KeyedMessage<String, String>("test", "www.wusyx.com" + System.currentTimeMillis());
+			producer.send(message );
+//			Thread.currentThread().sleep(1000);
+		}
+		
     }
 }
